2831344522cf40a61e0bd5a26ec69b4905fc5fa5
VistResearch <peter9503@gmail.com>
2019-09-10 21:39:03 +0800

    rename namespace due to php keyword

---
+++ src/Models/NEMnamespace/Alias.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace NEM\Models\Namespace;
+
+abstract class Alias{
+    /**
+     * The alias type
+     *
+     * - 0 : No alias
+     * - 1 : Mosaic id alias
+     * - 2 : Address alias
+     */
+    public $type; // number;
+
+    /**
+     * The alias address
+     */
+    public $address; // Address;
+
+    /**
+     * The alias mosaicId
+     */
+    public $mosaicId; // MosaicId;
+}
---
+++ src/Models/NEMnamespace/AliasType.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The alias type. Supported types are:
+ * 0: No alias.
+ * 1: Mosaic id alias.
+ * 2: Address alias.
+ *
+ * @since 0.10.2
+ */
+namespace NEM\Models\Namespace;
+
+class AliasType {
+    const None = 0;
+    const Mosaic = 1;
+    const Address = 2;
+}
