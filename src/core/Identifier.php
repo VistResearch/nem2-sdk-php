77fac9eecb38ade76e60a62ce7408ca294e65b78
VistResearch <peter9503@gmail.com>
2019-10-20 18:26:47 +0800

    update id generater

--- src/core/Identifier.php
+++ src/core/Identifier.php
@@ -5,8 +5,32 @@ namespace NEM\Core;
 use NEM\Models\Mosaic\MosaicNonce;
 use NEM\Core\Format\Convert as Convert;
 
+use Exception;
 
 class Identifier{
+	static function packUint8(Array $input): Array{
+		if(sizeof($input)%4 != 0){
+			throw new Exception("wrong format uint8 array to pack\n");
+		}
+
+		$output = [];
+		$t = 0;
+		for($i = 0; $i<sizeof($input); $i += 1){
+			$t += $input[$i] << (8 *($i%4));
+			if($i%4 == 3){
+				array_push($output, $t);
+				$t = 0;
+			}
+		}
+		return $output;
+	}
+
+	static function validNamesapceName(string $name): bool{
+
+
+		return ctype_alnum($name);
+	}
+
 	static function generateMosaicId(MosaicNonce $nonce, string $pbkey): Array{
 		$data = array_merge($nonce->nonce , unpack("C*",hex2bin($pbkey) ) );
 			$result = hash('sha3-256', pack("C*" , ...$data));
@@ -22,56 +46,52 @@ class Identifier{
 	}
 
 	static function generateNamespaceId(string $name): Array{
-		$name = "." . $name;
 		$names = explode('.', $name);
 
-
 		if ($name == ''){
+			throw new Exception("Namespace name " . $name . " has zero length\n");
 			return [];
 		}
 		else if(sizeof($names) > 3){
-        	throw Error("Namespace has too many parts.");
+        	throw new Exception("Namespace has too many parts.");
 		}
 
-	    $ids = [];
-	    $parent_id = 0;
-	    foreach ($names as $key => $name) {
-	     	$parent_id = self::generateSubNamespaceId($parent_id, $name);
-	        array_merge($ids,$parent_id);
-	     } 
+	    $namespaceId = [0,0,0,0,0,0,0,0]; // This is the base namesapceId
+	    $path = [];
+	    $outputPath = [];
+	    for ($index = 0; $index < sizeof($names); $index += 1) {
 
-	    return $ids;
+
+	    	if( !(self::validNamesapceName($names[$index])) ){
+	    		throw new Exception("Invalid namespace name ".$names[$index]."\n");	    		
+	    	}
+
+	    	$namespaceId = self::generateSubNamespaceId($namespaceId, $names[$index]);
+
+
+	    	array_push($path,$namespaceId);
+	    } 
+
+	    for ($index = 0; $index < sizeof($path); $index += 1) {
+	    	array_push($outputPath,self::packUint8($path[$index]));
+	    }
+	    return $outputPath[sizeof($outputPath)-1];
 	}
 
 	static function generateSubNamespaceId(Array $parentId, string $name): Array{
+		
+		$parentIdStr = pack("C*",...$parentId);
 
-		$IdArray = [($parentId[0] >> 0) & 0xff,
-					($parentId[0] >> 8) & 0xff,
-					($parentId[0] >> 16) & 0xff,
-					($parentId[0] >> 24) & 0xff,
-					($parentId[1] >> 0) & 0xff,
-					($parentId[1] >> 8) & 0xff,
-					($parentId[1] >> 16) & 0xff,
-					($parentId[1] >> 24) & 0xff];
+		$h = hash_init('sha3-256');
+		hash_update($h, $parentIdStr);
+		hash_update($h, $name);
+		$resultArray = Convert::hexToUint8(hash_final($h));
 
-		$nameArray = unpack('C*', $name);
+		$outputArray = array_slice($resultArray,0,8);
 
-		$data = array_merge($IdArray ,$nameArray);
+		$outputArray[7] |= 128;
 
-
-		$hash = hash('sha3-256', pack("C*" , ...$data));
-
-		// $hash = hash('sha3-256', $name);
-
-		var_dump(Convert::uint8ToUint32(Convert::HexToUint8($hash)));
-		// little endian
-		$tmp0 = unpack("H*", strrev(pack("H*", substr($hash,0,16))));
-		$tmp0 = hexdec($tmp0[1]);
-
-		$tmp1 = unpack("H*", strrev(pack("H*", substr($hash,0,16))));
-		$tmp1 = hexdec($tmp1[1]);
-
-	    return [$tmp0, $tmp1 & 0x80000000];
+	    return $outputArray;
 
 	}
 
