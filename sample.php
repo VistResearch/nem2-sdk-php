2195f071fc6946e58d586ccecea4937d5f4ad121
VistResearch <peter9503@gmail.com>
2019-10-26 15:10:42 +0800

    update sample

--- sample.php
+++ sample.php
@@ -17,9 +17,12 @@ $generationHash = '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B
 $sampleDeadline = Deadline::createFromDTO([0, 0]);
 $sampleAddress= Address::createFromRawAddress("NB3KUBHATFCPV7UZQLWAQ2EUR6SIHBSBEOEDDDF3");
 $sampleMosaicId = new MosaicId([3294802500, 2243684972]);
-$sampleMosaic = new Mosaic($MM, UInt64::fromUint(2));
+$sampleMosaic = new Mosaic($sampleMosaicId, UInt64::fromUint(2));
 $sampleMessage = PlainMessage::create('sample message');
-$sampleTx = TransferTransaction::create($D,$AD,[$M],$P,NetworkType::MIJIN_TEST);
+$sampleTx = TransferTransaction::create($sampleDeadline,$sampleAddress,[$sampleMosaic],$sampleMessage,NetworkType::MIJIN_TEST);
 
 $sampleAccount = Account::createFromPrivateKey($prkey,NetworkType::MIJIN_TEST,"SHA3");
 var_dump($sampleTx->signWith($sampleAccount,$generationHash));
+
+
+
